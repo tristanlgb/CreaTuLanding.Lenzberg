471dd66c65970c7d624de0e33589cadfe9abc30b
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2024-11-26 22:57:53 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MyNavbar from './components/MyNavbar';
+import ItemListContainer from './components/ItemListContainer';
+
+const App = () => {
+  return (
+    <>
+      <MyNavbar />
+      <ItemListContainer />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/MyNavbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import CartWidget from './CartWidget';
+
+const MyNavbar = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">MyStore</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/products">Products</Nav.Link>
+          </Nav>
+          <CartWidget />
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default MyNavbar;
